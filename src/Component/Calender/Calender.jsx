7f1f60f6d0c27b7c3555b7327944bedd18b7f66c
harshita <apslodhi23@gmail.coi>
2024-04-21 13:28:34 +0530

    harshita | added month filter

--- src/Component/Calender/Calender.jsx
+++ src/Component/Calender/Calender.jsx
@@ -1,39 +1,58 @@
+import CALENDER_CONSTANT from "./Calender.constants";
 import "./Calender.css";
 
-const Calender = () => {
+const Calender = ({ date }) => {
   const arrDay = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+  let monthLength = CALENDER_CONSTANT.DAYS_IN_MONTHS[date.month];
+  if (date.month === 1) {
+    if (
+      (date.year % 4 === 0 && date.year % 100 !== 0) ||
+      date.year % 400 === 0
+    ) {
+      monthLength = 29;
+    }
+  }
+  const firstDay = new Date(date.year, date.month, 1).getDay();
+  const totalDays =
+    firstDay >= 5 && CALENDER_CONSTANT.EXTRA_DAYS.includes(date?.month)
+      ? 42
+      : 35;
+  const dateList = Array.from(
+    { length: totalDays },
+    (_, i) => i + -firstDay + 1
+  );
 
-  const dateList = Array.from({ length: 35 }, (_, i) => i + 1);
-
-  const today = new Date().getDate();
+  const today = {
+    date: new Date().getDate(),
+    month: new Date().getMonth(),
+    year: new Date().getFullYear(),
+  };
 
   return (
     <>
-      <div className="container">
-        <div className="month-year box">
-          <button className="month-button">APR</button>
-          <button className="year-button">2024</button>
-        </div>
+      <div className="calender">
+        {arrDay.map((day, index) => (
+          <div key={index} className="day">
+            {day}
+          </div>
+        ))}
 
-        <div className="calender">
-          {arrDay.map((day, index) => (
-            <div key={index} className="day">
-              {day}
-            </div>
-          ))}
-
-          {dateList.map((date, index) => (
-            <div
-              key={index}
-              className={`date ${date === today ? "today" : ""}`}
-            >
-              {date}
-            </div>
-          ))}
-        </div>
-
-        <div className="holidays box"></div>
+        {dateList.map((item, index) => (
+          <div
+            key={index}
+            className={`date ${
+              item === today?.date &&
+              date?.month === today?.month &&
+              date?.year === today?.year
+                ? "today"
+                : ""
+            }`}
+          >
+            {item > 0 && item <= monthLength ? item : ""}
+          </div>
+        ))}
       </div>
+      <div className="holidays box"></div>
     </>
   );
 };
--- src/Component/Calender/Filter.jsx
+++ src/Component/Calender/Filter.jsx
@@ -1,4 +1,64 @@
-const Filter = () => {
-  return <></>;
+import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+import CALENDER_CONSTANT from "./Calender.constants";
+const Filter = ({ date, changeDate }) => {
+  const months = Array.from(
+    { length: CALENDER_CONSTANT.TOTAL_MONTHS },
+    (_, i) => i
+  );
+  const years = Array.from(
+    { length: CALENDER_CONSTANT.TOTAL_YEARS },
+    (_, i) => i + 2019
+  );
+
+  const handleChange = (event, label) => {
+    changeDate(event.target.value, label);
+  };
+
+  return (
+    <>
+      <div className="month-year box">
+        <FormControl>
+          <InputLabel id="month">Month</InputLabel>
+          <Select
+            labelId="month"
+            id="year-select"
+            value={date?.month === null ? "" : date.month}
+            label="month"
+            onChange={(event) =>
+              handleChange(event, CALENDER_CONSTANT.FILTER_TYPE.MONTH)
+            }
+          >
+            {months?.map((month) => {
+              return (
+                <MenuItem value={month} key={month}>
+                  {CALENDER_CONSTANT.MONTHS[month]}
+                </MenuItem>
+              );
+            })}
+          </Select>
+        </FormControl>
+        <FormControl>
+          <InputLabel id="year">Year</InputLabel>
+          <Select
+            labelId="year"
+            id="year-select"
+            value={date?.year ? date.year : ""}
+            label="year"
+            onChange={(event) =>
+              handleChange(event, CALENDER_CONSTANT.FILTER_TYPE.YEAR)
+            }
+          >
+            {years?.map((year) => {
+              return (
+                <MenuItem value={year} key={year}>
+                  {year}
+                </MenuItem>
+              );
+            })}
+          </Select>
+        </FormControl>
+      </div>
+    </>
+  );
 };
 export default Filter;
